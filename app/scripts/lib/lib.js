152081689b48a8159cce0299e22a0f2ef6e231c9
Mickael Faivre-Macon <faivrem@gmail.com>
2014-02-18 15:34:08 +0100

    adding persons quotes

--- app/scripts/lib/lib.js
+++ app/scripts/lib/lib.js
@@ -1,21 +1,20 @@
 'use strict';
 
 var key = 'AIzaSyDFVeNzDuEmGe7eZProsCUwxgthSfFU2Hs';
-var url = 'https://www.googleapis.com/freebase/v1'
+var url = 'https://www.googleapis.com/freebase/v1';
 
 function searchTopics(search) {
-	console.log('searchTopics: ' + search);
+    console.log('searchTopics: ' + search);
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url + '/search?query=' + search +'&key='+key, false);
+    xmlHttp.open("GET", url + '/search?query=' + search + '&key=' + key, false);
     xmlHttp.send(null);
     return eval("(" + xmlHttp.responseText + ")");
 }
 
 function searchTopic(id) {
-	console.log('searchTopic: ' + id);
+    console.log('searchTopic: ' + id);
     var xmlHttp = new XMLHttpRequest();
-    xmlHttp.open("GET", url + '/topic' + id +'?key='+key, false);
+    xmlHttp.open("GET", url + '/topic' + id + '?key=' + key, false);
     xmlHttp.send(null);
     return eval("(" + xmlHttp.responseText + ")");
 }
-
