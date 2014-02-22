7bef804c09ee62a773dcae9f1fbbeef6676bfec0
Mickael Faivre-Macon <faivrem@gmail.com>
2014-02-22 16:33:30 +0100

    fix mid

--- app/scripts/controllers/search.js
+++ app/scripts/controllers/search.js
@@ -20,8 +20,48 @@ angular.module('lifebitsApp')
 
     $rootScope.user = Google.getUser();
 
+
+    // TODO:
+    // 1. make a service
+    // 2 use $.ajax and asynchronous call
+    // 3. profit
+
+    var freebaseKey = 'AIzaSyDFVeNzDuEmGe7eZProsCUwxgthSfFU2Hs';
+    var url = 'https://www.googleapis.com/freebase/v1';
+
+    function doSearchTopics(search) {
+      console.log('searchTopics: ' + search);
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open("GET", url + '/search?query=' + search + '&key=' + freebaseKey, false);
+      xmlHttp.send(null);
+      return eval("(" + xmlHttp.responseText + ")");
+    }
+
+    function doSearchTopic(id) {
+      console.log('searchTopic: ' + id);
+      var xmlHttp = new XMLHttpRequest();
+      xmlHttp.open("GET", url + '/topic' + id + '?key=' + freebaseKey, false);
+      xmlHttp.send(null);
+      return eval("(" + xmlHttp.responseText + ")");
+      /*
+        $.ajax({ // FIXME: use Angular's $ressource ?
+            url: url + '/topic' + id + '?key=' + freebaseKey,
+            beforeSend: function (xhr) {
+              //xhr.setRequestHeader('Authorization', "OAuth " + token);
+              xhr.setRequestHeader('Accept',        "application/json");
+            },
+            success: function (response) {
+              callback(response);
+            }
+        });
+*/
+    }
+
     function doSearch(id) {
       $rootScope.details = doSearchTopic(id);
+      Db.getShares(id, function(shares) {
+        $rootScope.shares = shares;
+      });
       $rootScope.title = $rootScope.details.property['/type/object/name'].values[0].value;
       $rootScope.id = id;
       History.add(id, $rootScope.title);
@@ -48,7 +88,7 @@ angular.module('lifebitsApp')
     }).bind("fb-select", function(e, data) {
       console.log(data);
       $rootScope.$apply(function() {
-        doSearch(data.id);
+        doSearch(data.mid);
       });
     });
 
