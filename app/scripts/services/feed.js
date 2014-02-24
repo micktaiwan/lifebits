12289058917f55bd77dbdc3eaa67595066ea08d2
Mickael Faivre-Macon <faivrem@gmail.com>
2014-02-24 18:27:04 +0100

    news in service and filter for news and shares

--- app/scripts/services/feed.js
+++ app/scripts/services/feed.js
@@ -1,12 +1,29 @@
 'use strict';
 
-angular.module('lifebitsApp.services.feed', []).factory('Feed', ['$http',
-  function($http) {
+angular.module('lifebitsApp.services.feed', []).factory('Feed',
+  function($http, $rootScope) {
+
+    function safeApply(scope, fn) {
+      (scope.$$phase || scope.$root.$$phase) ? fn() : scope.$apply(fn);
+    };
+
+    var googleNewsUrl = 'https://news.google.com/news/feeds?output=rss&q=';
+
+    function parseFeed(url) {
+      return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+    };
 
     return {
-      parseFeed: function(url) {
-        return $http.jsonp('//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=50&callback=JSON_CALLBACK&q=' + encodeURIComponent(url));
+
+      loadFeed: function(search, callback) {
+        console.log('loading feed for ' + search);
+        parseFeed(googleNewsUrl + search).then(function(res) {
+          safeApply($rootScope, function() {
+            callback(res.data.responseData.feed.entries);
+          });
+        });
       }
+
     }
-  }
-]);
+
+  });
