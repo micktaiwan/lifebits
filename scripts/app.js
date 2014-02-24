cff2ba2180e78a504cf73d8764a43827aa13b1d7
Mickael Faivre-Macon <faivrem@gmail.com>
2014-02-24 13:39:12 +0100

    Updates

--- scripts/app.js
+++ scripts/app.js
@@ -1,46 +1,47 @@
 'use strict';
 
 angular.module('lifebitsApp', [
-    'ngCookies',
-    'ngResource',
-    'ngSanitize',
-    'ngRoute',
-    'google',
-    'lifebitsApp.services.db',
-    'lifebitsApp.services.history',
-    'lifebitsApp.filters.dateDelta',
-    'lifebitsApp.filters.partition',
-  ])
-    .config(function($routeProvider) {
-        $routeProvider
-            .when('/main', {
-                templateUrl: 'views/main.html',
-                controller: 'MainCtrl'
-            })
-            .when('/bits/id/:topicId*', {
-                templateUrl: 'views/bits.html',
-                controller: 'BitsCtrl'
-            })
-            .when('/bits/share/:shareId*', {
-                templateUrl: 'views/share.html',
-                controller: 'BitsCtrl'
-            })
-            .when('/bits', {
-                templateUrl: 'views/bits.html',
-                controller: 'BitsCtrl'
-            })
-            .when('/login', {
-                templateUrl: 'views/login.html',
-                controller: 'LoginCtrl'
-            })
-            .when('/:params', {
-                redirectTo: '/login'
-                /*
+  'ngCookies',
+  'ngResource',
+  'ngSanitize',
+  'ngRoute',
+  'google',
+  'lifebitsApp.services.db',
+  'lifebitsApp.services.history',
+  'lifebitsApp.services.feed',
+  'lifebitsApp.filters.dateDelta',
+  'lifebitsApp.filters.partition',
+])
+  .config(function($routeProvider) {
+    $routeProvider
+      .when('/main', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl'
+      })
+      .when('/bits/id/:topicId*', {
+        templateUrl: 'views/bits.html',
+        controller: 'BitsCtrl'
+      })
+      .when('/bits/share/:shareId*', {
+        templateUrl: 'views/share.html',
+        controller: 'BitsCtrl'
+      })
+      .when('/bits', {
+        templateUrl: 'views/bits.html',
+        controller: 'BitsCtrl'
+      })
+      .when('/login', {
+        templateUrl: 'views/login.html',
+        controller: 'LoginCtrl'
+      })
+      .when('/:params', {
+        redirectTo: '/login'
+        /*
                 templateUrl: 'views/login.html',
                 controller: 'LoginCtrl'
                 */
-            })
-            .otherwise({
-                redirectTo: '/main'
-            });
-    });
+      })
+      .otherwise({
+        redirectTo: '/main'
+      });
+  });
