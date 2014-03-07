fd06c1e7bf529e8e92a871c2de2504b7357c03ca
Mickael Faivre-Macon <faivrem@gmail.com>
2014-03-07 11:29:36 +0100

    Updates

--- scripts/controllers/header.js
+++
@@ -1,8 +0,0 @@
-'use strict';
-
-angular.module('lifebitsApp')
-    .controller('HeaderCtrl', function HeaderController($scope, $location) {
-        $scope.isActive = function(viewLocation) {
-            return viewLocation === $location.path();
-        };
-    });
